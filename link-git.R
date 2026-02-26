9af10aaac5c19fb74541427ca233e49caf74f0b3
SalvaOsuna <salvadorosunacaballero@gmail.com>
2026-02-26 10:20:55 -0600

    Initial commit

---
+++ link-git.R
@@ -0,0 +1,9 @@
+#1. Introduce Yourself to Git
+library(usethis)
+#2. Generate a New Personal Access Token (PAT)
+use_git_config(user.name = "SalvaOsuna", user.email = "salvadorosunacaballero@gmail.com")
+create_github_token()
+#3. Store the Token in RStudio
+library(gitcreds)
+gitcreds_set()
+#4. Sync Your Project
